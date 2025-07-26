42de37064826255bb5ab982fb63be959d1f29d4b
Richard Cagle <rick.cagle@hey.com>
2025-07-26 17:40:32 -0500

    change defaults, add a session_mode and game name, moved prompt to a constant

--- supertextadventure-cli.rb
+++ supertextadventure-cli.rb
@@ -7,20 +7,22 @@ DATA = {
   player: {
     name: nil,
   },
+  session_mode: nil,
   game: {
+    name: nil,
     rooms: []
   }
 }
 
-def newline(x=2)
+def newline(x=1)
   x.times { puts }
 end
 
-def wait(x=2)
+def wait(x=1)
   sleep(x)
 end
 
-intro = <<~'INTRO'
+INTRO = <<~'INTRO'
 
 Welcome To
 _____________________________________________________
@@ -41,25 +43,133 @@ _____________________________________________________
 
 INTRO
 
-prompt = TTY::Prompt.new
+PROMPT = TTY::Prompt.new
 
-prompt.ok intro
+class Base
+  attr_accessor :prompt
+ 
+  def initialize
+    @prompt = PROMPT
+  end
 
-player_name = prompt.ask("What is your name, adventurer?".colorize(:light_blue))
+  def p
+    @prompt
+  end
 
-DATA[:player][:name] = player_name
+  def self.run
+    new.run
+  end
+end
 
+class Intro < Base
+  CHOICES = {
+    "build" => "Build an Adventure",
+    "play" => "Play a Game"
+  }
+
+  def run
+    p.ok(INTRO)
+
+    player_name = p.ask("What is your name, adventurer?".colorize(:light_blue))
+
+    DATA[:player][:name] = player_name
+
+    newline
+
+    p.ok("Welcome, #{DATA[:player][:name]}!")
+
+    newline
+    wait
+
+    selection = p.select("What would you like to do?", CHOICES.invert)
+
+    DATA[:session_mode] = selection
+
+    p.say("You chose to #{CHOICES[selection]}.".colorize(:yellow))
+
+    newline
+    wait
+  end
+end
+
+class GameBuilder < Base
+  def run
+    p.ok("Starting Game Builder...".colorize(:yellow))
+    newline
+    wait
+
+    p.say("Let's build your adventure, #{DATA[:player][:name]}!".colorize(:light_blue))
+    newline
+
+    name_game if DATA[:game][:name].nil?
+
+    build_process
+  end
+
+  def name_game
+    game_name = p.ask("What is the name of your game?".colorize(:light_blue))
+
+    DATA[:game][:name] = game_name.strip
+    p.ok("Game name set to '#{DATA[:game][:name]}'.")
+    newline
+  end
+
+  def build_process
+    action = p.select("What would you like to do?") do |menu|
+      menu.choice name: "Add a Room",  value: :add_room
+      menu.choice name: "List Rooms", value: :list_rooms
+      menu.choice name: "Exit Game Builder",  value: :exit_builder
+    end
+
+    case action
+    when :add_room
+      add_room
+    when :list_rooms
+      list_rooms
+    when :exit_builder
+      exit_builder
+    end
+  end
+
+  def add_room
+    p.say("We're working on this part".colorize(:yellow))
+    wait
+    newline
+    build_process
+  end
+
+  def list_rooms
+    if DATA[:game][:rooms].empty?
+      p.say("No rooms have been added yet.".colorize(:red))
+    else
+      p.ok("Current Rooms:")
+    end
+    wait
+    newline
+    build_process
+  end
+
+  def exit_builder
+    p.say("Exiting Game Builder...".colorize(:light_blue))
+    newline
+    wait
+  end
+end
+
+# Run The App
+
+Intro.run
+
+if DATA[:session_mode] == "build"
+  GameBuilder.run
+else
+  p.say("Game playing functionality is not yet implemented.".colorize(:red))
+end
+
+PROMPT.ok("Thank you for playing Super Text Adventure CLI!")
 newline
 
-prompt.ok("Welcome, #{DATA[:player][:name]}!")
+# Display the DATA hash for debugging purposes
+PROMPT.say("Current Session Data:".colorize(:light_blue))
 
-newline
-wait
-
-choices = { "build" => "Build an Adventure", "play" => "Play a Game"}
-selection = prompt.select("What would you like to do?", choices.invert)
-
-prompt.say("You chose to #{choices[selection]}.", color: :yellow)
-
-newline
-wait
+puts DATA
